3f06c61470b92d5d377087837ffad9daeb89adfc
rammyyadav-dot <rammy.yadav@gmail.com>
2026-03-18 11:07:29 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Suraj Kumar Singh</h1>
+      <p>Hospitality Professional | UAE</p>
+      <button>Contact Me</button>
+    </div>
+  );
+}
